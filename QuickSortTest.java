0f0a3b763fe96264366017d38e987da2c163e175
Alexis <55262670+alexDacoder19@users.noreply.github.com>
2021-05-14 21:22:25 -0700

    Add files via upload
    tester file for quicksort

---
+++ QuickSortTest.java
@@ -0,0 +1,26 @@
+package report;
+
+import static org.junit.Assert.*;
+
+import java.util.concurrent.ForkJoinPool;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class QuickSortTest {
+
+	@Test
+	public void test() {
+		int[] inputArray = { 7, 12, 19, 3, 18, 4, 2, 6, 15, 8, 10 };
+		int[] resultOne = {2, 3, 4, 6 ,7, 8 ,10, 12, 15, 18, 19};
+        int n = inputArray.length;
+        
+        ForkJoinPool pool = ForkJoinPool.commonPool();
+        QuickSortTest objectOne = new QuickSortTest();
+        
+        pool.invoke(new QuickSort(0, n - 1, inputArray));
+        Assert.assertArrayEquals(resultOne, inputArray);
+		
+	}
+
+}
