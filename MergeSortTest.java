a46f16205ec75c90edddfe6717b03103651db3c2
Alexis <55262670+alexDacoder19@users.noreply.github.com>
2021-05-15 22:29:42 -0700

    Add files via upload
    test file fixed for mergesort

--- MergeSortTest.java
+++ MergeSortTest.java
@@ -1,7 +1,13 @@
+
+
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ForkJoinPool;
 
@@ -10,9 +16,11 @@ public class MergeSortTest {
     @Test
     public void test() {
         ForkJoinPool pool = ForkJoinPool.commonPool();
+        List<Integer> output = new ArrayList<>(Arrays.asList(2, 3, 4, 6, 7, 8, 10, 12, 15, 18, 19));
+        
         MergeSortTest objectOne = new MergeSortTest();
         List<Integer> result = pool.invoke(new MergeSort<Integer>(Arrays.asList(7, 12, 19, 3, 18, 4, 2, 6, 15, 8, 10)));
-        Assert.assertArrayEquals(result,#not sure what to pass in here);
+        assertEquals(result, output);
     }
 
 }
